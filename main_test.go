4e4f97243880d610ea28270d70da3c5d802d9bbe
Осенчаков Роман <rongo@yandex.ru>
2025-06-30 00:26:22 +0300

    add new test

--- main_test.go
+++ main_test.go
@@ -126,6 +126,13 @@ func TestMaxChunks_Success(t *testing.T) {
 			},
 			want: 0,
 		},
+		{
+			name: "Число 23",
+			args: args{
+				data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
+			},
+			want: 23,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
